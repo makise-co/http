9b345558acb3bc80276d495ad5dfa86c11dfcd78
Dmitry K <coder1994@gmail.com>
2020-12-08 00:25:11 +0300

    Makise-Co Beta-3 Support

--- tests/HttpServerTest.php
+++ tests/HttpServerTest.php
@@ -25,6 +25,7 @@ class HttpServerTest extends TestCase
     public function testItWorks(): void
     {
         $_ENV['HTTP_WORKER_NUM'] = $_ENV['HTTP_REACTOR_NUM'] = 1;
+        $_SERVER['HTTP_WORKER_NUM'] = $_SERVER['HTTP_REACTOR_NUM'] = 1;
 
         $application = new Application(
             dirname(__DIR__),
@@ -63,6 +64,7 @@ class HttpServerTest extends TestCase
     public function testBootstrapperCalled(): void
     {
         $_ENV['HTTP_WORKER_NUM'] = $_ENV['HTTP_REACTOR_NUM'] = 1;
+        $_SERVER['HTTP_WORKER_NUM'] = $_SERVER['HTTP_REACTOR_NUM'] = 1;
 
         $application = new Application(
             dirname(__DIR__),
@@ -116,6 +118,7 @@ class HttpServerTest extends TestCase
     public function testServerShutdownOnWorkerStartError(): void
     {
         $_ENV['HTTP_WORKER_NUM'] = $_ENV['HTTP_REACTOR_NUM'] = 1;
+        $_SERVER['HTTP_WORKER_NUM'] = $_SERVER['HTTP_REACTOR_NUM'] = 1;
 
         $application = new Application(
             dirname(__DIR__),
